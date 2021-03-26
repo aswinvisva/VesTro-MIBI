3ac3ae016d43eb8c29b3eafc5d5bf942ea861f28
Aswin Visva <visvaaswin@yahoo.ca>
2021-03-26 10:30:25 -0400

    Updating base.py

--- mibipy/plotting/base.py
+++ mibipy/plotting/base.py
@@ -62,15 +62,15 @@ class BaseMIBIPlot:
         self.y = y
         self.x = x
 
-    def x(self):
-        for feed_idx in range(self.all_feeds_data.shape[0]):
-            idx = pd.IndexSlice
-            feed_name = self.all_feeds_metadata.loc[idx[feed_idx, 0], "Feed Name"]
-
-            feed_dir = "%s/%s" % (parent_dir, feed_name)
-            mkdir_p(feed_dir)
-
-            feed_features = self.all_samples_features.loc[self.all_samples_features["Data Type"] == feed_name]
+    # def x(self):
+    #     for feed_idx in range(self.all_feeds_data.shape[0]):
+    #         idx = pd.IndexSlice
+    #         feed_name = self.all_feeds_metadata.loc[idx[feed_idx, 0], "Feed Name"]
+    #
+    #         feed_dir = "%s/%s" % (parent_dir, feed_name)
+    #         mkdir_p(feed_dir)
+    #
+    #         feed_features = self.all_samples_features.loc[self.all_samples_features["Data Type"] == feed_name]
 
     def make_figure(self, **kwargs):
         """
@@ -92,9 +92,9 @@ class BaseMIBIPlot:
             mkdir_p(self.save_dir)
 
             if ax is not None:
-                ax.savefig(self.save_dir + '/%s.png' % self.fig_name, bbox_inches='tight')
+                ax.savefig(save_dir + '/%s.png' % figure_name, bbox_inches='tight')
             else:
-                plt.savefig(self.save_dir + '/%s.png' % self.fig_name, bbox_inches='tight')
+                plt.savefig(save_dir + '/%s.png' % figure_name, bbox_inches='tight')
 
         if self.show:
             plt.show()
